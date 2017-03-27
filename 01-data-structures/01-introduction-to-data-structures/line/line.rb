760b49fb10f8c4ab4de3951a98a030286fe0fcaa
Bailey <bailey.g.weathers@gmail.com>
2017-03-27 11:48:34 -0400

    Completed line exercises

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,37 @@ class Line
   end
 
   def join(person)
+    members << person
   end
 
   def leave(person)
+    members.slice!(members.index(person))
   end
 
   def front
+    members.first
   end
 
   def middle
+    members[(members.length / 2).round]
   end
 
   def back
+    members.last
   end
 
   def search(person)
+    if members.include?(person)
+      person
+    else
+      nil
+    end
   end
 
   private
 
   def index(person)
+    members.index(person)
   end
 
 end
